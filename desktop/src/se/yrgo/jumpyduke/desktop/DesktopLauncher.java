a501c78055c3a472749576051c3696b10c5a76c2
Magnus Lilja <magnus@connoisseur.se>
2022-01-24 11:08:30 +0100

    Initial commit

---
+++ desktop/src/se/yrgo/jumpyduke/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.yrgo.jumpyduke.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.yrgo.jumpyduke.DukeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DukeGame(), config);
+	}
+}
